9216fa2fc012d52d6ac3d1544d4652bdca1c780a
crucis-onishi <crucis@crucis.xsrv.jp>
2020-03-06 08:46:09 +0900

    add quiz.py proto

---
+++ quiz.py
@@ -0,0 +1,96 @@
+# 選択肢部分をパーツ化すべし
+
+import tkinter as tk
+import threading
+import time
+
+questions = [
+                {"問題文":"球磨川禊の過負荷は次のうちどれ？", "選択肢":["大嘘憑き", "手のひら孵し", "光化静翔", "致死武器"], "正解":1},
+                {"問題文":"安心院なじみのスキルは次のうちどれ？", "選択肢":["大嘘憑き", "手のひら孵し", "光化静翔"], "正解":2},
+                {"問題文":"ミスターアンノウンのスキルは次のうちどれ？", "選択肢":["テスト", "手のひら孵し", "光化静翔", "致死武器"], "正解":3}
+            ]
+
+ # tk.Frameを継承したApplicationクラスを作成
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+
+        # ウィンドウの設定
+        self.master.title("ウィンドウのタイトル")
+
+        # 変数定義
+        self.question_number = 0
+        self.sentence_text = tk.StringVar()
+
+        # 実行内容
+        self.pack()
+        self.create_widget()
+        self.create_question()
+
+    # create_widgetメソッドを定義
+    def create_widget(self):
+
+        # 背景画像
+        bg_img = tk.Canvas(self, width=800, height=600)
+        bg_img.pack()
+        self.haikei = tk.PhotoImage(file="haikei.png") # イメージは、必ずインスタンス変数に代入する必要があります。そうしないと、関数を抜けたとたん変数のメモリーが 回収され、イメージが消えてしまうからです。
+        bg_img.create_image(400, 300, image=self.haikei)
+
+        # 得点版
+        label = tk.Label(self, relief=tk.RIDGE, width=17, height=3, text="得点\n" + "33.03", font=("MSゴシック", "20", "bold"), bg="white")
+        label.place(x=500, y=10)
+
+    # create_questionメソッドを定義
+    def create_question(self):
+
+        # 出題フレーム
+        self.quiz_frame = tk.Frame(self, relief=tk.RIDGE, bd=2, width=500, height=600)
+        self.quiz_frame.place(x=0, y=0)
+
+        # 第〇問
+        question_number_label = tk.Label(self, relief=tk.RIDGE, width=28, height=1, anchor=tk.N, text="第" + str(self.question_number + 1) + "問", font=("MSゴシック", "20", "bold"), bg="white")
+        question_number_label.place(x=10, y=10)
+
+        # 問題文
+        self.sentence_text.set(questions[self.question_number]["問題文"]) # sentence_textに文字列代入
+        sentence = tk.Label(self, relief=tk.RIDGE, width=28, height=8,anchor=tk.NW, justify="left", wraplength=470,textvariable=self.sentence_text,font=("MSゴシック", "20", "bold"), bg="white")
+        sentence.place(x=10, y=60)
+
+        # 選択肢
+        for i,choices in enumerate(questions[self.question_number]["選択肢"]):
+            if i + 1 == questions[self.question_number]["正解"]:
+                correct_button = tk.Button(self, width=28, text=choices, font=("MSゴシック", "20", "bold"), bg="white", command=self.correct)
+                correct_button.place(x=10, y=350 + (i * 60))
+            else:
+                incorrect_button = tk.Button(self, width=28, text=choices, font=("MSゴシック", "20", "bold"), bg="white", command=self.incorrect)
+                incorrect_button.place(x=10, y=350 + (i * 60))
+
+    # 選択肢削除メソッドを定義
+    def destroy_choices(self,parent):
+        children = parent.winfo_children()
+        for child in children:
+            child.destroy()
+
+    # 選択肢ボタンが押された時の処理
+    def correct(self):
+        self.sentence_text.set("正解！")
+        self.after(1500, self.next)
+
+    def incorrect(self):
+        self.sentence_text.set("不正解！")
+        self.after(1500, self.next)
+
+    # 正解不正解表示後の処理
+    def next(self):
+        self.question_number += 1
+        print(self.question_number)
+
+        # 問題をアップデート
+        self.destroy_choices(self.quiz_frame)
+        self.create_question()
+
+# メインの処理
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = Application(master=root)
+    app.mainloop()
