c1958094a4de2f8059fda0d89bd04ad3539939f9
crucis-onishi <onishi@denno-sekai.com>
2020-03-10 05:27:42 +0900

    change font yugochic

--- quiz.py
+++ quiz.py
@@ -1,6 +1,4 @@
 import tkinter as tk
-import threading
-import time
 
 questions = [
                 {"問題文":"この本のタイトルはなんでしょう？", "選択肢":["Pythonの教科書", "ゼロから学ぶPython", "プログラミングはじめの一歩", "本は本でも食べられない本"], "正解":3},
@@ -14,7 +12,7 @@ class Application(tk.Frame):
         super().__init__(master)
 
         # ウィンドウの設定
-        self.master.title("ウィンドウのタイトル")
+        self.master.title("クイズPythonアカデミー")
 
         # 変数定義
         self.question_number = 0
@@ -32,40 +30,40 @@ class Application(tk.Frame):
     def create_widget(self):
 
         # 背景画像
-        bg_img = tk.Canvas(self, width=800, height=600)
-        bg_img.pack()
-        self.haikei = tk.PhotoImage(file="haikei.png") # イメージは、必ずインスタンス変数に代入する必要があります。そうしないと、関数を抜けたとたん変数のメモリーが 回収され、イメージが消えてしまうからです。
-        bg_img.create_image(400, 300, image=self.haikei)
+        canvas = tk.Canvas(self, width=800, height=600)
+        canvas.pack()
+        self.bg_img = tk.PhotoImage(file="haikei.png") # イメージは、必ずインスタンス変数に代入する必要があります。そうしないと、関数を抜けたとたん変数のメモリーが 回収され、イメージが消えてしまうからです。
+        canvas.create_image(400, 300, image=self.bg_img)
 
         # 得点版
-        label = tk.Label(self, relief=tk.RIDGE, width=17, height=3, textvariable=self.score_text, font=("MSゴシック", "20", "bold"), bg="white")
+        label = tk.Label(self, relief=tk.RIDGE, width=17, height=3, textvariable=self.score_text, font=("游ゴシック体", "20", "bold"), bg="white")
         label.place(x=500, y=10)
 
     # create_questionメソッドを定義
     def create_question(self):
 
         # 第〇問
-        question_number_label = tk.Label(self, relief=tk.RIDGE, width=28, height=1, anchor=tk.N, text="第" + str(self.question_number + 1) + "問", font=("MSゴシック", "20", "bold"), bg="white")
+        question_number_label = tk.Label(self, relief=tk.RIDGE, width=28, height=1, anchor=tk.N, text="第" + str(self.question_number + 1) + "問", font=("游ゴシック体", "20", "bold"), bg="white")
         question_number_label.place(x=10, y=10)
 
         # 問題文
         self.sentence_text.set(questions[self.question_number]["問題文"]) # sentence_textに文字列代入
-        sentence = tk.Label(self, relief=tk.RIDGE, width=28, height=8,anchor=tk.NW, justify="left", wraplength=470, textvariable=self.sentence_text,font=("MSゴシック", "20", "bold"), bg="white")
+        sentence = tk.Label(self, relief=tk.RIDGE, width=28, height=8,anchor=tk.NW, justify="left", wraplength=470, textvariable=self.sentence_text,font=("游ゴシック体", "20", "bold"), bg="white")
         sentence.place(x=10, y=60)
 
         # 選択肢
         for i,choices in enumerate(questions[self.question_number]["選択肢"]):
             if i + 1 == questions[self.question_number]["正解"]:
-                correct_button = tk.Button(self, width=28, text=choices, font=("MSゴシック", "20", "bold"), bg="white", command=self.correct)
+                correct_button = tk.Button(self, width=28, text=choices, font=("游ゴシック体", "20", "bold"), bg="white", command=self.correct)
                 correct_button.place(x=10, y=350 + (i * 60))
             else:
-                incorrect_button = tk.Button(self, width=28, text=choices, font=("MSゴシック", "20", "bold"), bg="white", command=self.incorrect)
+                incorrect_button = tk.Button(self, width=28, text=choices, font=("游ゴシック体", "20", "bold"), bg="white", command=self.incorrect)
                 incorrect_button.place(x=10, y=350 + (i * 60))
 
     # create_resultメソッドを定義
     def create_result(self):
         # 問題文
-        result = tk.Label(self, relief=tk.RIDGE, width=28, height=8,anchor=tk.CENTER, wraplength=470,text="問題は以上です。\nお疲れさまでした。",font=("MSゴシック", "20", "bold"), bg="white")
+        result = tk.Label(self, relief=tk.RIDGE, width=28, height=8,anchor=tk.CENTER, wraplength=470,text="問題は以上です。\nお疲れさまでした。",font=("游ゴシック体", "20", "bold"), bg="white")
         result.place(x=10, y=60)
 
 
