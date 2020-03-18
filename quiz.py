119fd1637d91a7a72bd351580fd1fb6f9ca8837f
crucis-onishi <onishi@denno-sekai.com>
2020-03-18 16:31:50 +0900

    edit quiz1,2

--- quiz.py
+++ quiz.py
@@ -1,11 +1,17 @@
-# 正解ボタンと不正解ボタンをそれぞれ作成するパターン
+# bindメソッドで押されたボタンのテキストを参照して正解不正解を判定するパターン
 
 import tkinter as tk
 
 questions = [
-                {"問題文":"この本のタイトルはなんでしょう？","選択肢":["Pythonの教科書", "ゼロから学ぶPython", "プログラミングはじめの一歩", "本は本でも食べられない本"], "正解":3},
-                {"問題文":"次のうちPythonに当てはまるのは？", "選択肢":["静的型付け言語", "動的型付け言語", "オレ的カッコつけ言語"], "正解":2},
-                {"問題文":"次のうちPythonに当てはまるのは？", "選択肢":["シンプルで扱いやすい汎用言語", "国産のプログラミング言語", "マークアップ言語の1つ", "ブラウザ上で動作するスクリプト言語"], "正解":1}
+                {"問題文":"この本のタイトルはなんでしょう？",
+                    "選択肢":["Pythonの教科書", "ゼロから学ぶPython", "プログラミングはじめの一歩", "本は本でも食べられない本"],
+                    "正解":"プログラミングはじめの一歩"},
+                {"問題文":"次のうちPythonに当てはまるのは？",
+                    "選択肢":["静的型付け言語", "動的型付け言語", "オレ的カッコつけ言語"],
+                    "正解":"動的型付け言語"},
+                {"問題文":"次のうちPythonに当てはまるのは？",
+                    "選択肢":["シンプルで扱いやすい汎用言語", "国産のプログラミング言語", "マークアップ言語の1つ", "ブラウザ上で動作するスクリプト言語"],
+                    "正解":"シンプルで扱いやすい汎用言語"}
             ]
 
  # tk.Frameを継承したApplicationクラスを作成
@@ -18,16 +24,13 @@ class Application(tk.Frame):
 
         # 変数定義
         self.question_number = 0
-        self.sentence_text = tk.StringVar()
         self.score = 0.00
         self.allocate_points = 100 / len(questions)
-        self.score_text = tk.StringVar()
-        self.score_text.set("得点\n0.00")
 
         # 実行内容
         self.pack()
         self.create_widget()
-        self.create_question()
+        self.create_buttons()
 
     # create_widgetメソッドを定義
     def create_widget(self):
@@ -39,29 +42,27 @@ class Application(tk.Frame):
         canvas.create_image(400, 300, image=self.bg_img)
 
         # 得点版
-        scoreboard = tk.Label(self, relief=tk.RIDGE, width=17, height=3, textvariable=self.score_text, font=("游ゴシック体", "20", "bold"), bg="white")
-        scoreboard.place(x=500, y=10)
-
-    # create_questionメソッドを定義
-    def create_question(self):
+        self.scoreboard = tk.Label(self, relief=tk.RIDGE, width=17, height=3, text="得点\n" + str(round(self.score, 2)), font=("游ゴシック体", "20", "bold"), bg="white")
+        self.scoreboard.place(x=500, y=10)
 
         # 第〇問
-        question_number_label = tk.Label(self, relief=tk.RIDGE, width=28, height=1, anchor=tk.N, text="第" + str(self.question_number + 1) + "問", font=("游ゴシック体", "20", "bold"), bg="white")
-        question_number_label.place(x=10, y=10)
+        self.question_number_label = tk.Label(self, relief=tk.RIDGE, width=28, height=1, anchor=tk.N, text="第" + str(self.question_number + 1) + "問", font=("游ゴシック体", "20", "bold"), bg="white")
+        self.question_number_label.place(x=10, y=10)
 
         # 問題文
-        self.sentence_text.set(questions[self.question_number]["問題文"]) # sentence_textに文字列代入
-        sentence = tk.Label(self, relief=tk.RIDGE, width=28, height=8,anchor=tk.NW, justify="left", wraplength=470, textvariable=self.sentence_text,font=("游ゴシック体", "20", "bold"), bg="white")
-        sentence.place(x=10, y=60)
+        self.sentence = tk.Label(self, relief=tk.RIDGE, width=28, height=8,anchor=tk.NW, justify="left", wraplength=470, text=questions[self.question_number]["問題文"], font=("游ゴシック体", "20", "bold"), bg="white")
+        self.sentence.place(x=10, y=60)
 
-        # 選択肢
+    # create_buttonsメソッドを定義
+    def create_buttons(self):
+
+        # 選択肢ボタン
+        self.buttons = []
         for i,choice in enumerate(questions[self.question_number]["選択肢"]):
-            if i + 1 == questions[self.question_number]["正解"]:
-                correct_button = tk.Button(self, width=28, text=choice, font=("游ゴシック体", "20", "bold"), bg="white", command=self.correct)
-                correct_button.place(x=10, y=350 + (i * 60))
-            else:
-                incorrect_button = tk.Button(self, width=28, text=choice, font=("游ゴシック体", "20", "bold"), bg="white", command=self.incorrect)
-                incorrect_button.place(x=10, y=350 + (i * 60))
+            button = tk.Button(self, width=28, text=choice, font=("游ゴシック体", "20", "bold"), bg="white")
+            button.bind("<Button-1>", self.button_clicked)
+            button.place(x=10, y=350 + (i * 60))
+            self.buttons.append(button)
 
     # create_resultメソッドを定義
     def create_result(self):
@@ -69,33 +70,41 @@ class Application(tk.Frame):
         result = tk.Label(self, relief=tk.RIDGE, width=28, height=8,anchor=tk.CENTER, wraplength=470,text="問題は以上です。\nお疲れさまでした。",font=("游ゴシック体", "20", "bold"), bg="white")
         result.place(x=10, y=60)
 
-    # フレームの子要素削除メソッドを定義
-    def destroy_widgets(self,parent):
-        children = parent.winfo_children()
-        for child in children:
-            child.destroy()
+    # ボタンを全てDISABLEDにする処理
+    def button_disabled(self):
+        for button in self.buttons:
+            button["state"] = tk.DISABLED
 
     # 選択肢ボタンが押された時の処理
-    def correct(self):
-        self.sentence_text.set("正解！")
-        self.score += self.allocate_points
-        self.score_text.set("得点\n" + str(round(self.score, 2)))
-        self.after(1500, self.next)
+    def button_clicked(self, event):
+        self.button_disabled()
+        if event.widget["text"] == questions[self.question_number]["正解"]:
+            self.sentence["text"] = "正解！"
+            self.score += self.allocate_points
+            self.scoreboard["text"] = "得点\n" + str(round(self.score, 2))
+        else:
+            self.sentence["text"] = "不正解！"
 
-    def incorrect(self):
-        self.sentence_text.set("不正解！")
         self.after(1500, self.next)
 
     # 正解不正解表示後の処理
     def next(self):
         self.question_number += 1
-        self.destroy_widgets(self) # フレームの子要素を全て削除
-        self.create_widget() # ウィジェットを再生成
+        self.destroy_buttons() # 選択肢ボタンをすべて削除
 
         if self.question_number < len(questions):
-            self.create_question() # 次の問題を描画
+            self.question_number_label["text"] = "第" + str(self.question_number + 1) + "問" # 何問目かを更新
+            self.sentence["text"] = questions[self.question_number]["問題文"] # 問題文を更新
+            self.create_buttons() # 次の問題を描画
         else:
+            self.question_number_label.destroy()
+            self.sentence.destroy()
             self.create_result() # リザルト画面を描画
+
+    # ボタンを削除する処理
+    def destroy_buttons(self):
+        for button in self.buttons:
+            button.destroy()
 
 # メインの処理
 if __name__ == "__main__":
--- quiz2.py
+++ quiz2.py
@@ -1,4 +1,4 @@
-# bindメソッドで押されたボタンのテキストを参照して正解不正解を判定するパターン
+# bindメソッドを使わずcommandでやるパターン（callback関数にテキストを渡す）
 
 import tkinter as tk
 
@@ -24,16 +24,13 @@ class Application(tk.Frame):
 
         # 変数定義
         self.question_number = 0
-        self.sentence_text = tk.StringVar()
         self.score = 0.00
         self.allocate_points = 100 / len(questions)
-        self.score_text = tk.StringVar()
-        self.score_text.set("得点\n0.00")
 
         # 実行内容
         self.pack()
         self.create_widget()
-        self.create_question()
+        self.create_buttons()
 
     # create_widgetメソッドを定義
     def create_widget(self):
@@ -45,26 +42,26 @@ class Application(tk.Frame):
         canvas.create_image(400, 300, image=self.bg_img)
 
         # 得点版
-        scoreboard = tk.Label(self, relief=tk.RIDGE, width=17, height=3, textvariable=self.score_text, font=("游ゴシック体", "20", "bold"), bg="white")
-        scoreboard.place(x=500, y=10)
-
-    # create_questionメソッドを定義
-    def create_question(self):
+        self.scoreboard = tk.Label(self, relief=tk.RIDGE, width=17, height=3, text="得点\n" + str(round(self.score, 2)), font=("游ゴシック体", "20", "bold"), bg="white")
+        self.scoreboard.place(x=500, y=10)
 
         # 第〇問
-        question_number_label = tk.Label(self, relief=tk.RIDGE, width=28, height=1, anchor=tk.N, text="第" + str(self.question_number + 1) + "問", font=("游ゴシック体", "20", "bold"), bg="white")
-        question_number_label.place(x=10, y=10)
+        self.question_number_label = tk.Label(self, relief=tk.RIDGE, width=28, height=1, anchor=tk.N, text="第" + str(self.question_number + 1) + "問", font=("游ゴシック体", "20", "bold"), bg="white")
+        self.question_number_label.place(x=10, y=10)
 
         # 問題文
-        self.sentence_text.set(questions[self.question_number]["問題文"]) # sentence_textに文字列代入
-        sentence = tk.Label(self, relief=tk.RIDGE, width=28, height=8,anchor=tk.NW, justify="left", wraplength=470, textvariable=self.sentence_text,font=("游ゴシック体", "20", "bold"), bg="white")
-        sentence.place(x=10, y=60)
+        self.sentence = tk.Label(self, relief=tk.RIDGE, width=28, height=8,anchor=tk.NW, justify="left", wraplength=470, text=questions[self.question_number]["問題文"], font=("游ゴシック体", "20", "bold"), bg="white")
+        self.sentence.place(x=10, y=60)
+
+    # create_buttonsメソッドを定義
+    def create_buttons(self):
 
         # 選択肢ボタン
+        self.buttons = []
         for i,choice in enumerate(questions[self.question_number]["選択肢"]):
-            button = tk.Button(self, width=28, text=choice, font=("游ゴシック体", "20", "bold"), bg="white")
-            button.bind("<Button-1>", self.button_clicked)
+            button = tk.Button(self, width=28, text=choice, font=("游ゴシック体", "20", "bold"), bg="white", command=self.button_clicked(choice))
             button.place(x=10, y=350 + (i * 60))
+            self.buttons.append(button)
 
     # create_resultメソッドを定義
     def create_result(self):
@@ -72,33 +69,43 @@ class Application(tk.Frame):
         result = tk.Label(self, relief=tk.RIDGE, width=28, height=8,anchor=tk.CENTER, wraplength=470,text="問題は以上です。\nお疲れさまでした。",font=("游ゴシック体", "20", "bold"), bg="white")
         result.place(x=10, y=60)
 
-    # 選択肢ボタンが押された時の処理
-    def button_clicked(self, event):
-        if event.widget["text"] == questions[self.question_number]["正解"]:
-            self.sentence_text.set("正解！")
-            self.score += self.allocate_points
-            self.score_text.set("得点\n" + str(round(self.score, 2)))
-        else:
-            self.sentence_text.set("不正解！")
+    # ボタンを全てDISABLEDにする処理
+    def button_disabled(self):
+        for button in self.buttons:
+            button["state"] = tk.DISABLED
 
-        self.after(1500, self.next)
+    # 選択肢ボタンが押された時の処理
+    def button_clicked(self, choice):
+        def x():
+            self.button_disabled()
+            if choice == questions[self.question_number]["正解"]:
+                self.sentence["text"] = "正解！"
+                self.score += self.allocate_points
+                self.scoreboard["text"] = "得点\n" + str(round(self.score, 2))
+            else:
+                self.sentence["text"] = "不正解！"
+
+            self.after(1500, self.next)
+        return x
 
     # 正解不正解表示後の処理
     def next(self):
         self.question_number += 1
-        self.destroy_widgets(self) # フレームの子要素を全て削除
-        self.create_widget() # ウィジェットを再生成
+        self.destroy_buttons() # 選択肢ボタンをすべて削除
 
         if self.question_number < len(questions):
-            self.create_question() # 次の問題を描画
+            self.question_number_label["text"] = "第" + str(self.question_number + 1) + "問" # 何問目かを更新
+            self.sentence["text"] = questions[self.question_number]["問題文"] # 問題文を更新
+            self.create_buttons() # 次の問題を描画
         else:
+            self.question_number_label.destroy()
+            self.sentence.destroy()
             self.create_result() # リザルト画面を描画
 
-    # フレームの子要素削除メソッドを定義
-    def destroy_widgets(self,parent):
-        children = parent.winfo_children()
-        for child in children:
-            child.destroy()
+    # ボタンを削除する処理
+    def destroy_buttons(self):
+        for button in self.buttons:
+            button.destroy()
 
 # メインの処理
 if __name__ == "__main__":
