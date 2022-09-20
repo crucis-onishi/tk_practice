de3d14b715b61c1f89815d89afd920677110c90d
crucis-onishi <onishi@denno-sekai.com>
2022-09-20 21:36:02 +0900

    test

--- test.py
+++ test.py
@@ -1,6 +1,4 @@
 import tkinter as tk
-import time
-import math
 
  # tk.Frameを継承したApplicationクラスを作成
 class Application(tk.Frame):
@@ -12,6 +10,7 @@ class Application(tk.Frame):
         master.geometry("430x280") # タイマーの幅は430x280
 
         # 変数定義
+        self.set_time = 0 # セット時間
         self.left_min = 0 # 残り時間（分）
         self.left_sec = 0 # 残り時間（秒）
 
@@ -31,24 +30,61 @@ class Application(tk.Frame):
         self.canvas_time.place(x=10, y=10)
 
         # タイマーに数字を表示
-        self.canvas_time.create_text(250,40,text=str(self.left_min).zfill(2) + "：", font=("MSゴシック体", "36", "bold"), tag="min_text", anchor="e") # 分を表示
-        self.canvas_time.create_text(250,40,text=str(self.left_sec).zfill(2), font=("MSゴシック体", "36", "bold"), tag="sec_text", anchor="w") # 秒を表示
+        self.update_min_text() # 分の表示更新
+        self.update_sec_text() # 秒の表示更新
 
         # 分ボタン
-        self.min_button = tk.Button(self.canvas_bg, width=8, height=2, text="分", font=("MSゴシック体", "18","bold"))
+        self.min_button = tk.Button(self.canvas_bg, width=8, height=2, text="分", font=("MSゴシック体", "18","bold"), command=self.min_button_clicked)
         self.min_button.place(x=10, y=100)
 
         # 秒ボタン
-        self.sec_button = tk.Button(self.canvas_bg, width=8, height=2, text="秒", font=("MSゴシック体", "18","bold"))
+        self.sec_button = tk.Button(self.canvas_bg, width=8, height=2, text="秒", font=("MSゴシック体", "18","bold"), command=self.sec_button_clicked)
         self.sec_button.place(x=150, y=100)
 
         # リセットボタン
-        self.reset_button = tk.Button(self.canvas_bg, width=8, height=2, text="リセット", font=("MSゴシック体", "18","bold"))
+        self.reset_button = tk.Button(self.canvas_bg, width=8, height=2, text="リセット", font=("MSゴシック体", "18","bold"), command=self.reset_button_clicked)
         self.reset_button.place(x=290, y=100)
 
         # スタート/ストップボタン
         start_button = tk.Button(self.canvas_bg, width=27, height=2, text="スタート/ストップ", font=("MSゴシック体", "18","bold"))
         start_button.place(x=10, y=190)
+
+# 各ボタンを押した時の処理
+
+    # minボタンを押した時
+    def min_button_clicked(self):
+        if self.left_min < 59: # 最大59分まで
+            self.set_time += 60 # セット時間をプラス
+            self.left_min += 1 # 残り時間（分）をプラス
+            self.update_min_text() # 分の表示更新
+
+    # secボタンを押した時
+    def sec_button_clicked(self):
+        if self.left_sec < 59: # 最大59秒まで
+            self.set_time += 1 # セット時間をプラス
+            self.left_sec += 1 # 残り時間（秒）をプラス
+            self.update_sec_text() # 秒の表示更新
+
+    # resetボタンを離した時
+    def reset_button_clicked(self):
+        self.set_time = 0 # セット時間をリセット
+        self.left_min = 0 # 残り時間（分）をリセット
+        self.left_sec = 0 # 残り時間（秒）をリセット
+
+        self.update_min_text() # 分の表示更新
+        self.update_sec_text() # 秒の表示更新
+
+# 個別の処理
+
+    # 分の表示更新
+    def update_min_text(self):
+        self.canvas_time.delete("min_text") # 表示時間（分）を消去
+        self.canvas_time.create_text(250, 40, text=str(self.left_min).zfill(2) + "：", font=("MSゴシック体", "36", "bold"), tag="min_text", anchor="e") # 分を表示
+
+    # 秒の表示更新
+    def update_sec_text(self):
+        self.canvas_time.delete("sec_text") # 表示時間（秒）を消去
+        self.canvas_time.create_text(250,40,text=str(self.left_sec).zfill(2), font=("MSゴシック体", "36", "bold"), tag="sec_text", anchor="w") # 秒を表示
 
 if __name__ == "__main__":
     root = tk.Tk()
